c075f5558ca2c696567b408a07320ed781f83cce
savikvsmbs23-collab <savikvsmbs.23@gmail.com>
2026-08-14 22:30:13 -0500

    Create vehicle.py

---
+++ oop/vehicle_system/vehicle.py
@@ -0,0 +1,10 @@
+class Vehicle:
+    def __init__(self, brand, speed):
+        self.brand = brand
+        self.speed = speed
+    def move(self):
+        return f"Vehicle is moving at {self.speed}km/h"
+
+
+
+    
